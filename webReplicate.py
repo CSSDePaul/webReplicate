78fe08a8836be96b307f68ec8115cd7ceab2ff85
Li-Wey Lu <liweylu@gmail.com>
2016-03-31 23:42:14 -0600

    Update

--- webReplicate.py
+++ webReplicate.py
@@ -3,36 +3,19 @@ import urllib
 import sys
 import os
 
-URL = raw_input("URL: ")
-PORT = int(raw_input("Port: "))
-illegal = '\/:*?"<>|'
-
-def removeIllegal(path):
-    for item in illegal:
-        path = path.replace(item,"_")
-    return path
-
-dirURL = removeIllegal(URL)
-if not os.path.exists(dirURL):
-    try:
-        urllib.urlopen(URL)
-        os.makedirs(dirURL)
-    except:
-        sys.exit()
-
 class myHandler(BaseHTTPRequestHandler):
     def do_GET(self):
         self.send_response(200)
         self.end_headers()
-        path = dirURL+"/"+removeIllegal(self.path)
+        path = directory+"/"+removeIllegal(self.path)
         if os.path.isfile(path):
             tempFile = open(path,"rb")
             content = tempFile.read()
             tempFile.close()
         else:
             try:
-                content = urllib.urlopen(URL+self.path).read()
-                urllib.urlretrieve(URL+self.path,path)
+                content = urllib.urlopen(url+self.path).read()
+                urllib.urlretrieve(url+self.path,path)
             except:
                 content =   '''
                             <html>
@@ -49,15 +32,32 @@ class myHandler(BaseHTTPRequestHandler):
                             </html>
                             '''
         self.wfile.write(content)
-        return
 
-try:
+def getServer(port):
     BaseHTTPRequestHandler.server_version = "Microsoft-IIS/8.0"
     BaseHTTPRequestHandler.sys_version = ""
-    server = HTTPServer(("",PORT), myHandler)
-    print "Replicating {} on port {}".format(URL,PORT)
+    return HTTPServer(("",port), myHandler)
+
+def verify(url,directory):
+    if not os.path.exists(directory):
+        try:
+            urllib.urlopen(url)
+            os.makedirs(directory)
+        except:
+            sys.exit()
+
+def removeIllegal(path):
+    for item in '\/:*?"<>|':
+        path = path.replace(item,"_")
+    return path
+
+if __name__ == "__main__":
+    url = raw_input("URL: ")
+    port = int(raw_input("Port: "))
+    directory = removeIllegal(url)
+    verify(url,directory)
+    server = getServer(port)
+    print "Replicating {} on port {}".format(url,port)
     server.serve_forever()
-except KeyboardInterrupt:
-    server.socket.close()
 
 
